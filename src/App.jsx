890124599b6300a520f8bd8663762b3e8321ef9a
SnehalSura <snehalsura@gmail.com>
2024-12-31 20:47:08 +0530

    remove the unwanted import statement

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { getColleges } from './App';
-import Lift from '../../Lift-Implementation/src/myComponents/Lift';
+
 
 function App() {
   let [clgList, setClgList] = useState([]);
